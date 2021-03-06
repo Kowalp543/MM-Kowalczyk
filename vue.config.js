588fa5c96aeae6904c5f8d2ca29bc2f9f297638b
Kowalp543 <piotr.kowalczyk1@op.pl>
2021-03-06 16:54:26 +0100

    end

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,18 @@
-module.exports = {   devServer: {     disableHostCheck: true   } }
+const path = require('path') // eslint-disable-line
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+module.exports = {
+  devServer: {
+    disableHostCheck: true
+  },
+
+  chainWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugin('prerender').use(PrerenderSPAPlugin, [
+        {
+          staticDir: path.join(__dirname, 'dist'),
+          routes: ['/', '/okulista', '/diabetolog', '/kontakt'],
+        },
+      ])
+    }
+  },
+}
