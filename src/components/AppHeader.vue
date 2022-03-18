037f1a7f2339d6e84e9645ab762b9df4567572d1
Kowalp543 <piotr.kowalczyk1@op.pl>
2022-03-19 00:42:59 +0100

    mobile menu 2

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -242,25 +242,25 @@ body{
 } */
 
 .push{
-    display: flex;
+    display: inline;
 }
 
- .bm-burger-button {
-      top: 31px;
-      cursor: pointer;
-      height: 22px;
-      width: 30px;
-    }
-    .bm-burger-bars {
-        background-color: #e4bc9b;
-        border-radius: 30px;
-    }
-    .line-style {
-      position: absolute;
-      height: 20%;
-      left: 0;
-      right: 0;
-    }
+.bm-burger-button {
+    top: 31px;
+    cursor: pointer;
+    height: 22px;
+    width: 30px;
+}
+.bm-burger-bars {
+    background-color: #e4bc9b;
+    border-radius: 30px;
+}
+.line-style {
+    position: absolute;
+    height: 20%;
+    left: 0;
+    right: 0;
+}
     /* .cross-style {
       position: absolute;
       top: 12px;
@@ -275,13 +275,13 @@ body{
       width: 24px;
     } */
     .bm-menu {
-      height: 100%; /* 100% Full-height */
+      height: 34%; /* 100% Full-height */
       width: 0; /* 0 width - change this with JavaScript */
       position: fixed; /* Stay in place */
       z-index: 1000; /* Stay on top */
       top: 0;
       left: 0;
-      background-color: #FAF8F6; /* Black*/
+      background-color:rgb(63, 63, 65); /* #FAF8F6*/
       overflow-x: hidden; /* Disable horizontal scroll */
       padding-top: 60px; /* Place content 60px from the top */
       transition: 0.5s; /*0.5 second transition effect to slide in the sidenav*/
